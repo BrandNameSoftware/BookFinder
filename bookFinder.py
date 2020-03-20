6a4243d07cc1c04f5034ef1a57f89b6d57e2e274
Matthew Brand <mbrand@gmail.com>
2020-03-20 06:55:54 -0600

    Got search of denver working
    Not sure if it's counting the data properly, but at least it's returning results.

--- bookFinder.py
+++ bookFinder.py
@@ -5,6 +5,9 @@ import pandas as pd
 import sys
 import string
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def retrieve_book_listing_from_url(url):
     desiredTitle = url[0]
@@ -17,24 +20,20 @@ def retrieve_book_listing_from_url(url):
         "numOther" : 0
     }
     for libraryURL in librarySearchURLs:
-        driver = webdriver.Firefox()
-        driver.get('https://catalog.denverlibrary.org/search/searchresults.aspx?ctx=1.1033.0.0.6&type=Keyword&term=How%20Not%20to%20Die&by=TI&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1')
-        driver.implicitly_wait(1000)
-
-        soup = BeautifulSoup(driver.page_source,'html.parser')
-        print(soup)
-
-
-        #session = requests.Session()
-        #r = session.get("https://catalog.denverlibrary.org/search/searchresults.aspx?ctx=1.1033.0.0.6&type=Keyword&term=How%20Not%20to%20Die&by=TI&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1")
-        #soup = BeautifulSoup(r.text,'html.parser')
-        #with open("libraryResults.html", "w") as file:
-        #    file.write(str(soup))
-        #print(soup)
         if "bibliocommons" in libraryURL:
+            r = requests.get(url[1])
+            soup = BeautifulSoup(r.text,'html.parser')
             results = extract_books_from_bibliocommons_result(soup,desiredTitle, results)
             results["jeffCoURL"] = libraryURL
         else:
+            broswer = webdriver.Firefox()
+            try:
+                broswer.get(libraryURL)
+                WebDriverWait(broswer, 5).until(EC.presence_of_element_located( ( By.ID, "searchResultsDIV" ) ))
+                soup = BeautifulSoup(broswer.page_source,'html.parser')
+            finally:
+                #this will eat the timeout exception that happens when no search results are found
+                broswer.quit()
             results = extract_books_from_denver_result(soup, desiredTitle, results)
             results["denverURL"] = libraryURL
     return results
@@ -42,7 +41,6 @@ def retrieve_book_listing_from_url(url):
 def extract_books_from_denver_result(soup, desiredTitle, existingData):
     existingData.update({"denverURL" : ""})
     for book in soup.find_all('span', attrs={'class':'nsm-e135'}):
-        print("found class")
         try:
             #extract a short title (bibliocommons only returns the short title)
             baseTitle = book.text.split(':')[0].split('(')[0].split('!')[0].strip()
